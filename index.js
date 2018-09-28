65a78d85a904deec33ed8c457e98d0880e1f775a
Juho G <juho.gebhard@gmail.com>
2018-09-28 18:54:36 +0700

    express-ejs-layouts

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 require('dotenv').config()
 
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts');
 const helmet = require('helmet')
 const mainRouter = require('./src/routes/mainRouter')
 
 const app = express()
 
-app.set('views', './src/views/pages')
+app.set('views', './src/views')
 app.set('view engine', 'ejs')
+app.set('layout', 'layouts/layout')
+
+app.use(expressLayouts);
 
 // Serve public files.
 app.use(express.static('public'))
