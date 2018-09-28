205903cf4da84a5595ece0907d88a09027967f3c
Juho G <juho.gebhard@gmail.com>
2018-09-28 12:33:35 +0700

    Quote api

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
+require('./quote_api/model/db');
+
+var routesApi = require('./quote_api/routes/index');
 
 const express = require('express')
 const helmet = require('helmet')
 const mainRouter = require('./src/routes/mainRouter')
 
+
 const app = express()
+
 
 app.set('views', './src/views/pages')
 app.set('view engine', 'ejs')
@@ -22,6 +27,9 @@ app.use(express.urlencoded({ extended: true }))
 // Router of the main page.
 app.use('/', mainRouter)
 
+// Router of the api
+app.use('/api', routesApi);
+
 // Handle 404.
 app.use((req, res) => res.sendStatus(404))
 
@@ -34,3 +42,5 @@ app.use((err, req, res, next) => {
 app.listen(8080)
 
 // console.dir(json, {colors: true, depth: null})
+
+// curl -d '{"query": "Juho", "title": "Trumpf", "quote": "greatest Trump quote!", "image_url": "www.my_image.org"}' -H "Content-Type: application/json" -X POST http://locahost:8080/api/quotes
---
+++ quote_api/routes/index.js
@@ -0,0 +1,28 @@
+// var ctrlQuotes = require('../controllers/quotes');
+
+var express = require('express');
+var router = express.Router();
+
+var ctrlQuotes = require('../controllers/quotes');
+
+router.get('/quotes', ctrlQuotes.quoteReadAll);
+router.post('/quotes', ctrlQuotes.quoteCreate);
+
+
+
+// var ctrlLocations = require('../controllers/locations');
+// var ctrlReviews = require('../controllers/reviews');
+
+// router.get('/locations', ctrlLocations.locationsListByDistance);
+// router.post('/locations', ctrlLocations.locationsCreate);
+// router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
+// router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
+// router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
+
+// reviews
+// router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
+// router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
+// router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
+// router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
+
+module.exports = router;
